9418de655ada28ed60c22c042016a5faf75f99d5
Vanya <38669449+morozvanya1@users.noreply.github.com>
2023-01-27 21:02:31 +0300

    fix menu

--- js/script.js
+++ js/script.js
@@ -5,14 +5,18 @@ musicArtist = document.querySelector(".song-details .artist"),
 mainAudio = wrapper.querySelector("#main-audio"),
 playPauseBtn = wrapper.querySelector(".play-pause"),
 prevBtn = wrapper.querySelector("#prev"),
-nextBtn = wrapper.querySelector("#next");
-progressBar = document.querySelector(".progress-bar");
+nextBtn = wrapper.querySelector("#next"),
+progressBar = wrapper.querySelector(".progress-bar"),
+progressArea = wrapper.querySelector(".progress-area"),
+musicList = wrapper.querySelector(".music-list"),
+showMoreBtn = wrapper.querySelector("#more-music"),
+hideMusicBtn = wrapper.querySelector("#close");
 
 let musicIndex = 2;
 
 window.addEventListener("load", ()=>{
     loadMusic(musicIndex);
-})
+});
 
 function loadMusic(indexNumb) {
     musicName.innerText = allMusic[indexNumb - 1].name;
@@ -87,3 +91,104 @@ mainAudio.addEventListener("timeupdate", (e)=>{
     }
     musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
 });
+
+progressArea.addEventListener("click", (e)=>{
+    let progressWidthval = progressArea.clientWidth;
+    let clickedOffSetX = e.offsetX;
+    let songDuration = mainAudio.duration;
+
+    mainAudio.currentTime = (clickedOffSetX / progressWidthval) * songDuration;
+    playMusic();
+});
+
+const repeatBtn = wrapper.querySelector("#repeat-plist");
+repeatBtn.addEventListener("click", (e)=>{
+    let getText = repeatBtn.innerText;
+    switch(getText) {
+        case "repeat":
+            repeatBtn.innerText = "repeat_one";
+            break;
+        case "repeat_one":
+            repeatBtn.innerText = "shuffle";
+            break;
+        case "shuffle":
+            repeatBtn.innerText = "repeat";
+            break;
+    }
+});
+
+mainAudio.addEventListener("ended", ()=>{
+    let getText = repeatBtn.innerText;
+    switch(getText) {
+        case "repeat":
+            nextMusic();
+            break;
+        case "repeat_one":
+            mainAudio.currentTime = 0;
+            loadMusic(musicIndex);
+            playMusic();
+            break;
+        case "shuffle":
+            let randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+            do {
+                randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+            } while (musicIndex == randIndex);
+            musicIndex = randIndex;
+            loadMusic(musicIndex);
+            playMusic();
+            break;
+    }
+});
+
+showMoreBtn.addEventListener("click", ()=>{
+    musicList.classList.toggle("show");
+});
+
+hideMusicBtn.addEventListener("click", ()=>{
+    showMoreBtn.click();
+});
+
+const ulTag = wrapper.querySelector("ul");
+
+for (let i = 0; i < allMusic.length; i++) {
+    let liTag = `<li li-index="${i}">
+                    <div class="row">
+                        <span>${allMusic[i].name}</span>
+                        <p>${allMusic[i].artist}</p>
+                    </div>
+                    <audio class="${allMusic[i].src}" src="songs/${allMusic[i].src}.mp3"></audio>
+                    <span id="${allMusic[i].src}" class="audio-duration"></span>
+                </li>`;
+    ulTag.insertAdjacentHTML("beforeend", liTag);
+
+    let liAudioTagDuration = ulTag.querySelector(`#${allMusic[i].src}`);
+    let liAudioTag = ulTag.querySelector(`.${allMusic[i].src}`);
+
+    liAudioTag.addEventListener("loadeddata", ()=>{
+        let audioDuration = liAudioTag.duration;
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        if (totalSec < 10) {
+            totalSec = `0${totalSec}`;
+        }
+        liAudioTagDuration.innerText = `${totalMin}:${totalSec}`;
+    });
+
+}
+
+const allLiTags = ulTag.querySelectorAll("li");
+for (let j = 0; j < allLiTags.length; j++) {
+
+    if (allLiTags[j].getAttribute("li-index") == musicIndex) {
+        allLiTags[j].classList.add("playing");
+    }
+    
+    allLiTags[j].setAttribute("onclick", "clicked(this)")
+}
+
+function clicked(el) {
+    let getLinIndex = el.getAttribute("li-index");
+    musicIndex = getLinIndex;
+    loadMusic(musicIndex);
+    playMusic();
+}
