53c190e255d500233cd22579eaf60da6c259968b
Vanya <38669449+morozvanya1@users.noreply.github.com>
2023-01-27 19:38:14 +0300

    First commit

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name: "+44",
+        artist: "LIL KRYSTALLL",
+        img: "02-big",
+        src: "lil_44"
+    },
+    {
+        name: "Логан",
+        artist: "LIL KRYSTALLL",
+        img: "03-big",
+        src: "lil_logan"
+    },
+    {
+        name: "Я буду",
+        artist: "LIL KRYSTALLL",
+        img: "01-big",
+        src: "lil_be"
+    },
+    {
+        name: "Mazzaleen",
+        artist: "LIL KRYSTALLL",
+        img: "04-big",
+        src: "lil_Mazzaleen"
+    },
+    {
+        name: "Матильда",
+        artist: "LIL KRYSTALLL",
+        img: "05-big",
+        src: "lil_matilda"
+    },
+    {
+        name: "Шенген",
+        artist: "LIL KRYSTALLL",
+        img: "06-big",
+        src: "lil_shena"
+    }
+]
